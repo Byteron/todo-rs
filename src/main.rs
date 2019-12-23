36d2f9954e5b1d2c6b46096579f0dc2452d5000f
Aaron Winter <winter.aaron93@gmail.com>
2019-12-23 22:10:31 +0100

    cool stuff

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use std::env;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
 mod todo;
 
 use todo::TodoList;
 
 fn main() {
+
+	let mut list = load();
+
 	let args: Vec<String> = env::args().collect();
+
+	if args.len() < 2 {
+		return
+	}
+
 	let command: String = args[1].clone();
-
-	let mut list = TodoList::new();
-
-	list.add_new("This is Task One");
-	list.add_new("This is Task Two");
 
 	match command.as_str() {
 		"list" => {
@@ -20,20 +25,52 @@ fn main() {
 		"add" => {
 			let task: String = args[2].clone();
 			list.add_new(task.as_str());
-			list.print();
 		}
-		"complete" => {
+		"tick" => {
 			let task: String = args[2].clone();
 			let task_id: i32 = task.trim().parse().unwrap();
 			list.mark_completed(task_id, true);
-			list.print();
 		}
-		"incomplete" => {
+		"untick" => {
 			let task: String = args[2].clone();
 			let task_id: i32 = task.trim().parse().unwrap();
 			list.mark_completed(task_id, false);
-			list.print();
+		}
+		"delete" => {
+			let task: String = args[2].clone();
+			let task_id: i32 = task.trim().parse().unwrap();
+			list.remove(task_id);
+		}
+		"reset" => {
+			list.reset();
 		}
 		_ => { println!("Unknown Command: {}", command); }
 	}
+
+	save(list);
+}
+
+fn load() -> TodoList {
+	let mut file = OpenOptions::new()
+		.read(true).write(true).create(true)
+		.open("todo.txt")
+		.unwrap();
+	
+	let mut contents = String::new();	
+	file.read_to_string(&mut contents).expect("Could not read file");
+
+	let list = TodoList::from(contents);
+
+	list
+}
+
+fn save(list: TodoList) {
+	let mut file = OpenOptions::new()
+	.read(true).write(true).create(true)
+	.open("todo.txt")
+	.unwrap();
+
+	file.set_len(0).expect("Could not erase file");
+	file.write_all(list.to_string().as_ref()).expect("Could not write to file.");
+	file.sync_all().expect("Could not sync file.");
 }
--- src/todo.rs
+++ src/todo.rs
@@ -7,8 +7,8 @@ pub struct TodoItem {
 }
 
 impl TodoItem {
-	pub fn new(id: i32, description: &str) -> Self {
-		TodoItem { id, description: description.to_string(), completed: false }
+	pub fn new(id: i32, description: &str, completed: bool) -> Self {
+		TodoItem { id, description: description.to_string(), completed }
 	}
 
 	pub fn print(&self) {
@@ -31,20 +31,77 @@ impl TodoList {
 		TodoList { counter: 0i32, list: Vec::new() }
 	}
 
+	pub fn from(file_content: String) -> Self {
+		let mut list = TodoList::new();
+
+		if file_content.is_empty() {
+			return list;
+		}
+
+		let split: Vec<&str> = file_content.split("\n").collect();
+		
+		{
+			let data: &[&str] = &split[1..split.len()];
+			
+			for string in data.iter() {
+				if string.is_empty() {
+					continue
+				}
+				
+				let split: Vec<&str> = string.split("::").collect();
+
+				let id: i32 = split[0].parse().unwrap();
+				let description = split[1];
+				let completed: bool = split[2].parse().unwrap();
+				
+				let item = TodoItem::new(id, description, completed);
+				
+				list.add(item);
+			}
+		}
+
+		let counter: i32 = split[0].parse().unwrap();
+		list.counter = counter;
+
+		list
+	}
+	
 	pub fn add(&mut self, item: TodoItem) {
 		self.list.push(item);
 		self.counter += 1;
 	}
 
 	pub fn add_new(&mut self, description: &str) {
-		let item = TodoItem::new(self.counter, description);
+		let item = TodoItem::new(self.counter, description, false);
 		self.add(item);
+	}
+
+	pub fn remove(&mut self, id: i32) {
+		let index = self.list.iter().position(|item| item.id == id).unwrap();
+		self.list.remove(index);
+	}
+
+	pub fn reset(&mut self) {
+		self.list = Vec::new();
+		self.counter = 0;
 	}
 
 	pub fn mark_completed(&mut self, id: i32, completed: bool) {
 		self.list.iter_mut()
 			.filter(|item| item.id == id)
 			.for_each(|item| item.completed = completed);
+	}
+
+	pub fn to_string(&self) -> String {
+		let mut string = String::new();
+
+		string.push_str(format!("{}\n", self.counter).as_ref());
+		
+		for item in self.list.iter() {
+			string.push_str(format!("{}::{}::{}\n", item.id, item.description, item.completed).as_ref());
+		}
+
+		string
 	}
 
 	pub fn print(&self) {
