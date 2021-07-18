a839c58cac4f2d8404d49bf601443365a51dcaf9
Aaron Winter <winter.aaron93@gmail.com>
2021-07-18 12:57:45 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,67 @@ use std::env;
 use todo::Command;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	
-	if args.len() < 2 { return }
+    let args: Vec<String> = env::args().collect();
 
-	let mut list = todo::load();
+    if args.len() < 2 {
+        return;
+    }
 
-	let command = match args[1].as_str() {
-		"list" => Command::List,
-		"add" => Command::Add(args[2].clone()),
-		"tick" => Command::Tick(args[2].clone().trim().parse().expect("Failed to parse Argument")),
-		"untick" => Command::Untick(args[2].clone().trim().parse().expect("Failed to parse Argument")),
-		"remove" => Command::Remove(args[2].clone().trim().parse().expect("Failed to parse Argument")),
-		"reset" => Command::Reset,
-		"exit" => Command::Exit,
-		_ => Command::Unknown(args[1].clone()),
-	};
+    let mut list = todo::load();
 
-	match command {
-		Command::List => {
-			list.print();
-		}
-		Command::Add(task) => {
-			list.add_new(task.as_str());
-		}
-		Command::Tick(task_id) => {
-			list.mark_completed(task_id, true);
-		}
-		Command::Untick(task_id) => {
-			list.mark_completed(task_id, false);
-		}
-		Command::Remove(task_id) => {
-			list.remove(task_id);
-		}
-		Command::Reset => {
-			list.reset();
-		}
-		Command::Unknown(command) => {
-			println!("Unknown Command {}", command);
-		}
-		Command::Exit => {},
-	}
+    let command = match args[1].as_str() {
+        "list" => Command::List,
+        "add" => Command::Add(args[2].clone()),
+        "tick" => Command::Tick(
+            args[2]
+                .clone()
+                .trim()
+                .parse()
+                .expect("Failed to parse Argument"),
+        ),
+        "untick" => Command::Untick(
+            args[2]
+                .clone()
+                .trim()
+                .parse()
+                .expect("Failed to parse Argument"),
+        ),
+        "remove" => Command::Remove(
+            args[2]
+                .clone()
+                .trim()
+                .parse()
+                .expect("Failed to parse Argument"),
+        ),
+        "reset" => Command::Reset,
+        "exit" => Command::Exit,
+        _ => Command::Unknown(args[1].clone()),
+    };
 
-	todo::save(list);
+    match command {
+        Command::List => {
+            list.print();
+        }
+        Command::Add(task) => {
+            list.add_new(task.as_str());
+        }
+        Command::Tick(task_id) => {
+            list.mark_completed(task_id, true);
+        }
+        Command::Untick(task_id) => {
+            list.mark_completed(task_id, false);
+        }
+        Command::Remove(task_id) => {
+            list.remove(task_id);
+        }
+        Command::Reset => {
+            list.reset();
+        }
+        Command::Unknown(command) => {
+            println!("Unknown Command {}", command);
+        }
+        Command::Exit => {}
+    }
+
+    todo::save(list);
 }
--- src/todo.rs
+++ src/todo.rs
@@ -2,149 +2,165 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 
 pub enum Command {
-	List,
-	Add(String),
-	Remove(i32),
-	Tick(i32),
-	Untick(i32),
-	Reset,
-	Exit,
-	Unknown(String),
+    List,
+    Add(String),
+    Remove(i32),
+    Tick(i32),
+    Untick(i32),
+    Reset,
+    Exit,
+    Unknown(String),
 }
 
 #[derive(Debug)]
 pub struct TodoItem {
-	pub id: i32,
-	pub description: String,
-	pub completed: bool,
+    pub id: i32,
+    pub description: String,
+    pub completed: bool,
 }
 
 impl TodoItem {
-	pub fn new(id: i32, description: &str, completed: bool) -> Self {
-		TodoItem { id, description: description.to_string(), completed }
-	}
+    pub fn new(id: i32, description: &str, completed: bool) -> Self {
+        TodoItem {
+            id,
+            description: description.to_string(),
+            completed,
+        }
+    }
 
-	pub fn print(&self) {
-		println!(
-			"#{} [{}] - {}", 
-			self.id,
-			if self.completed { "x" } else { " " }, 
-			self.description
-		);
-	}
+    pub fn print(&self) {
+        println!(
+            "#{} [{}] - {}",
+            self.id,
+            if self.completed { "x" } else { " " },
+            self.description
+        );
+    }
 }
 
 pub struct TodoList {
-	pub counter: i32,
-	pub list: Vec<TodoItem>,
+    pub counter: i32,
+    pub list: Vec<TodoItem>,
 }
 
 impl TodoList {
-	pub fn new() -> Self {
-		TodoList { counter: 0i32, list: Vec::new() }
-	}
+    pub fn new() -> Self {
+        TodoList {
+            counter: 0i32,
+            list: Vec::new(),
+        }
+    }
 
-	pub fn from(file_content: String) -> Self {
-		let mut list = TodoList::new();
+    pub fn from(file_content: String) -> Self {
+        let mut list = TodoList::new();
 
-		if file_content.is_empty() {
-			return list;
-		}
+        if file_content.is_empty() {
+            return list;
+        }
 
-		let split: Vec<&str> = file_content.split("\n").collect();
-		
-		{
-			let data: &[&str] = &split[1..split.len()];
-			
-			for string in data.iter() {
-				if string.is_empty() {
-					continue
-				}
-				
-				let split: Vec<&str> = string.split("::").collect();
+        let split: Vec<&str> = file_content.split("\n").collect();
 
-				let id: i32 = split[0].parse().unwrap();
-				let description = split[1];
-				let completed: bool = split[2].parse().unwrap();
-				
-				let item = TodoItem::new(id, description, completed);
-				
-				list.add(item);
-			}
-		}
+        {
+            let data: &[&str] = &split[1..split.len()];
 
-		let counter: i32 = split[0].parse().unwrap();
-		list.counter = counter;
+            for string in data.iter() {
+                if string.is_empty() {
+                    continue;
+                }
 
-		list
-	}
-	
-	pub fn add(&mut self, item: TodoItem) {
-		self.list.push(item);
-		self.counter += 1;
-	}
+                let split: Vec<&str> = string.split("::").collect();
 
-	pub fn add_new(&mut self, description: &str) {
-		let item = TodoItem::new(self.counter, description, false);
-		self.add(item);
-	}
+                let id: i32 = split[0].parse().unwrap();
+                let description = split[1];
+                let completed: bool = split[2].parse().unwrap();
 
-	pub fn remove(&mut self, id: i32) {
-		let index = self.list.iter().position(|item| item.id == id).unwrap();
-		self.list.remove(index);
-	}
+                let item = TodoItem::new(id, description, completed);
 
-	pub fn reset(&mut self) {
-		self.list = Vec::new();
-		self.counter = 0;
-	}
+                list.add(item);
+            }
+        }
 
-	pub fn mark_completed(&mut self, id: i32, completed: bool) {
-		self.list.iter_mut()
-			.filter(|item| item.id == id)
-			.for_each(|item| item.completed = completed);
-	}
+        let counter: i32 = split[0].parse().unwrap();
+        list.counter = counter;
 
-	pub fn to_string(&self) -> String {
-		let mut string = String::new();
+        list
+    }
 
-		string.push_str(format!("{}\n", self.counter).as_ref());
-		
-		for item in self.list.iter() {
-			string.push_str(format!("{}::{}::{}\n", item.id, item.description, item.completed).as_ref());
-		}
+    pub fn add(&mut self, item: TodoItem) {
+        self.list.push(item);
+        self.counter += 1;
+    }
 
-		string
-	}
+    pub fn add_new(&mut self, description: &str) {
+        let item = TodoItem::new(self.counter, description, false);
+        self.add(item);
+    }
 
-	pub fn print(&self) {
-		for item in &self.list {
-			item.print();
-		}
-	}
+    pub fn remove(&mut self, id: i32) {
+        let index = self.list.iter().position(|item| item.id == id).unwrap();
+        self.list.remove(index);
+    }
+
+    pub fn reset(&mut self) {
+        self.list = Vec::new();
+        self.counter = 0;
+    }
+
+    pub fn mark_completed(&mut self, id: i32, completed: bool) {
+        self.list
+            .iter_mut()
+            .filter(|item| item.id == id)
+            .for_each(|item| item.completed = completed);
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut string = String::new();
+
+        string.push_str(format!("{}\n", self.counter).as_ref());
+
+        for item in self.list.iter() {
+            string.push_str(
+                format!("{}::{}::{}\n", item.id, item.description, item.completed).as_ref(),
+            );
+        }
+
+        string
+    }
+
+    pub fn print(&self) {
+        for item in &self.list {
+            item.print();
+        }
+    }
 }
 
 pub fn load() -> TodoList {
-	let mut file = OpenOptions::new()
-		.read(true).write(true).create(true)
-		.open("todo.txt")
-		.unwrap();
-	
-	let mut contents = String::new();	
-	file.read_to_string(&mut contents).expect("Could not read file");
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("todo.txt")
+        .unwrap();
 
-	let list = TodoList::from(contents);
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Could not read file");
 
-	list
+    let list = TodoList::from(contents);
+
+    list
 }
 
 pub fn save(list: TodoList) {
-	let mut file = OpenOptions::new()
-	.read(true).write(true).create(true)
-	.open("todo.txt")
-	.unwrap();
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("todo.txt")
+        .unwrap();
 
-	file.set_len(0).expect("Could not erase file");
-	file.write_all(list.to_string().as_ref()).expect("Could not write to file.");
-	file.sync_all().expect("Could not sync file.");
+    file.set_len(0).expect("Could not erase file");
+    file.write_all(list.to_string().as_ref())
+        .expect("Could not write to file.");
+    file.sync_all().expect("Could not sync file.");
 }
